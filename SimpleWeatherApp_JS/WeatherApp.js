4317c54731d8f584d6da70615dfa1a5baa04cba7
Jose Salas-Ayala <84983102+Jsalasay@users.noreply.github.com>
2022-05-05 13:24:33 -0400

    Update WeatherApp.js

--- SimpleWeatherApp_JS/WeatherApp.js
+++ SimpleWeatherApp_JS/WeatherApp.js
@@ -11,72 +11,70 @@ $( document ).ready(function() {
    //on loadup
    $("#userLocation").click(function(){
       navigator.geolocation.getCurrentPosition(function(position){
+         $(".msg").html("");
          let lat = position.coords.latitude;
          let long = position.coords.longitude;
          
          $.ajax({
             url: 'https://api.openweathermap.org/data/2.5/weather',
-            data: {lat: lat, lon: long, units: "imperial", appid: "e493d7860e9f31ea12032a54ec9425f4"}
-         }).done(function(data){
-             const {main, name, sys, weather } = data;
-             const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
-
-             const li = document.createElement("li");
-            li.classList.add("city");
-            const markup = `
-               <h2 class="city-name" data-name="${name},${sys.country}">
-                  <span>${name}</span>
-                  <sup>${sys.country}</sup>
-               </h2>
-               <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
-               </div>
-               <figure>
-                  <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-                  <figcaption>${weather[0]["description"]}</figcaption>
-               </figure>
-            `;
-         li.innerHTML = markup;
-         $(".cities").append(li);
-         }).fail(function(jqXHR){
-            $(".msg").html("There was a problem contacting the server: " + jqXHR.status + " " + jqXHR.responseText);
+            data: {lat: lat, lon: long, units: "imperial", appid: "e493d7860e9f31ea12032a54ec9425f4"},
+            success: function(data){
+               const {main, name, sys, weather } = data;
+               const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+  
+               const li = document.createElement("li");
+              li.classList.add("city");
+              const markup = `
+                 <h2 class="city-name" data-name="${name},${sys.country}">
+                    <span>${name}</span>
+                    <sup>${sys.country}</sup>
+                 </h2>
+                 <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
+                 </div>
+                 <figure>
+                    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+                    <figcaption>${weather[0]["description"]}</figcaption>
+                 </figure>
+              `;
+           li.innerHTML = markup;
+           $(".cities").append(li);
+           },
+           error: function(jqXHR){$(".msg").html("There was a problem contacting the server: " + jqXHR.status + " " + jqXHR.responseText);}
          });
-         $(".msg").html("");
+         
 
       });
    });
    
 
-   $("button").click(function() {
+   $("#search").click(function() {
       
        let city = $("#city").val();
        $.ajax({
            url: "http://api.openweathermap.org/data/2.5/weather",
-           data: { q: city, units: "imperial", appid: 
-   "e493d7860e9f31ea12032a54ec9425f4" }
-        }).done(function(data) {
-         const {main, name, sys, weather } = data;
-         const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+           data: { q: city, units: "imperial", appid: "e493d7860e9f31ea12032a54ec9425f4" },
+           success: function(data){
+            const {main, name, sys, weather } = data;
+            const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
 
-         const li = document.createElement("li");
-        li.classList.add("city");
-        const markup = `
-           <h2 class="city-name" data-name="${name},${sys.country}">
-              <span>${name}</span>
-              <sup>${sys.country}</sup>
-           </h2>
-           <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
-           </div>
-           <figure>
-              <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-              <figcaption>${weather[0]["description"]}</figcaption>
-           </figure>
-        `;
-     li.innerHTML = markup;
-     $(".cities").append(li);
-       }).fail(function(jqXHR) {
-          $(".msg").html("There was a problem contacting the server: " +
-             jqXHR.status + " " + jqXHR.responseText);
-       });
-       $(".msg").html("");
+            const li = document.createElement("li");
+           li.classList.add("city");
+           const markup = `
+              <h2 class="city-name" data-name="${name},${sys.country}">
+                 <span>${name}</span>
+                 <sup>${sys.country}</sup>
+              </h2>
+              <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
+              </div>
+              <figure>
+                 <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+                 <figcaption>${weather[0]["description"]}</figcaption>
+              </figure>
+           `;
+        li.innerHTML = markup;
+        $(".cities").append(li);
+        },
+        error: function(jqXHR){$(".msg").html("There was a problem contacting the server: " + jqXHR.status + " " + jqXHR.responseText);}
+        });
     });
 });
